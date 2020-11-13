4569fea3026fa4c7713412c518b1c6c17630b9f6
Rodrigo de Souza <rsouza01@gmail.com>
2020-11-13 15:58:25 +0000

    Added Dotenv lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(__dirname, '..', '/.env') });
+
+console.log(`  HOST => ${process.env.HOST}`);
